d29921b9aad65267443ec226380624226d227e39
Sarah Hall <swatson1994@yahoo.com>
2023-02-15 02:59:18 -0500

    Add tests for PB section and update index view

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -12,7 +12,28 @@ RSpec.describe 'Home Page', type:feature do
   it 'has the correct title' do
     within '#welcomeHeader' do
       expect(page).to have_text("Hello, and welcome to Sai's Porfolio!") 
+    end  
+  end
+
+  context "Professional Background section" do
+
+    it 'has a section for my professional background text' do
+      expect(page).to have_selector("section#professionalBackground")
     end
-  
+
+    it 'has a title in this section: PB' do
+      within '#professionalBackground' do
+        expect(page).to have_selector("h3#pb-Title")
+        expect(page).to have_text("My Professional Background")
+      end
+    end
+
+    it 'has a section for me to write about my professional background' do
+      within '#professionalBackground' do
+        expect(page).to have_selector("p#professionalBackgroundText")
+        expect(page).to have_text("Temporary Test Text -PB")
+        
+      end
+    end
   end
 end
