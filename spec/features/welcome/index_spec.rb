22e7b8f288008b213f3c129c2e9f0abc457834f4
Sarah Hall <swatson1994@yahoo.com>
2023-02-15 02:31:45 -0500

    Add test and title for home page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Home Page', type:feature do
-  it 'can be navigated to' do
+  before(:each) do
     visit "/"
-    expect(page).to have_content("Test Page")
+  end
+  it 'can be navigated to' do
+    expect(page.status_code).to eq(200)
+    expect(page).to have_current_path("/")
+  end
+
+  it 'has the correct title' do
+    within '#welcomeHeader' do
+      expect(page).to have_text("Hello, and welcome to Sai's Porfolio!") 
+    end
+  
   end
 end
