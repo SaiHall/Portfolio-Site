d587383ee0cc60e9f05ed16e79270e2e509bfd0d
Sarah Hall <swatson1994@yahoo.com>
2023-02-22 19:20:09 -0500

    Add tests for title and preview section, update view

--- spec/features/blog/index_spec.rb
+++ spec/features/blog/index_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe 'Blog Index Page', type:feature do
     expect(page.status_code).to eq(200)
     expect(page).to have_current_path("/blog")
   end
+
+  it 'has a title' do
+    within '#blogHeader' do
+      expect(page).to have_text("Sai's Blog!")
+    end
+  end
+
+  it 'has a section for blog previews' do
+    expect(page).to have_selector('#postPreviews')
+  end
 end
