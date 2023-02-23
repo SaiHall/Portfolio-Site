56caccf5eb77223a8741fb86ee89e9bb14c0d163
Sarah Hall <swatson1994@yahoo.com>
2023-02-22 19:12:15 -0500

    Add route, controller, and blank view for blog index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
+
+  get '/blog', to: 'blog#index'
   # root "articles#index"
 end
